918fb511fa6022e63b9dbdcbcf417e91165f2f31
Lawson Kurtz <lawson.kurtz@viget.com>
2015-04-07 14:32:45 -0600

    Correct File#download and File#delete paths

--- lib/egnyte/file.rb
+++ lib/egnyte/file.rb
@@ -20,11 +20,11 @@ module Egnyte
     # :content_length_proc
     # :progress_proc
     def stream( opts={} )
-      @session.streaming_download( "#{fs_path('fs-content')}/#{path}", opts )
+      @session.streaming_download( "#{fs_path('fs-content')}#{path}", opts )
     end
 
     def delete
-      @session.delete("#{fs_path}/#{path}")
+      @session.delete("#{fs_path}#{path}")
     end
 
     def self.find(session, path)
@@ -33,7 +33,7 @@ module Egnyte
       file = File.new({
         'path' => path
       }, session)
-      
+
       parsed_body = session.get("#{file.fs_path}#{path}")
 
       raise FileExpected if parsed_body['is_folder']
